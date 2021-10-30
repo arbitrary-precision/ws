0baa4f0566cec82431364307326e7b4fec5c9672
Volodymyr Zakalyk <volodymyr.zakalyk@gmail.com>
2021-10-31 01:30:44 +0300

    Add Boost.Multiprecision dependency.
    Version 1.77.0.
    
    Retrieved with bcp command. From boost release directory run:
     ./bootstrap.sh
     ./b2 tools/bcp
     ./dist/bin/bcp boost/multiprecision/cpp_int.hpp DEST_DIR
    Where DEST_DIR - any folder.

--- src/sandbox/sandbox.hpp
+++ src/sandbox/sandbox.hpp
@@ -2,7 +2,10 @@
 #define DEOHAYER_SANDBOX_HPP
 
 #include <ap/ap.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
 #include <iostream>
 #include <vector>
+
+#define bmp boost::multiprecision
 
 #endif
